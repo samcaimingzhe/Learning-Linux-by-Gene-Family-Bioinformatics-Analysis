ee0427b6552eb18c5c27f7a7e665666817b69896
Mingzhe Cai 蔡明哲 <pyrus.cai@outlook.com>
2026-03-05 21:31:35 +0100

    Create transform_gff_len.py

---
+++ transform_gff_len.py
@@ -0,0 +1,31 @@
+#!usr/bin/env python
+# conding: utf-8
+import sys
+import pandas as pd
+
+data = pd.read_csv(sys.argv[1], sep="\t", header=None)
+data = data[data[2] == 'mRNA']
+
+data['id'] = data[8].str.split(';').str[0].str.replace('ID=', '', regex=False).str.split('.').str[0]
+
+data['len'] = data[4]-data[3]
+
+for name, group in data.groupby(['id']):
+    if len(group) == 1:
+        continue
+    ind = group.sort_values(by='len', ascending=False).index[1:].values
+    data.drop(index=ind, inplace=True)
+
+data['order'] = ''
+data['newname'] = ''
+data[3] = data[3].astype('int')
+
+for name, group in data.groupby([0]):
+    group = group.sort_values(by=[2])
+    data.loc[group.index, 'order'] = list(range(1, len(group) + 1))
+data = data[[0,'id',3,4,6,'order',8]]
+
+data = data.sort_values(by=[0, 'order'])
+data.to_csv(sys.argv[2], sep="\t", index=False, header=None)
+lens = data.groupby(0).max()[[3, 'order']]
+lens.to_csv(sys.argv[3], sep="\t", header=None)
